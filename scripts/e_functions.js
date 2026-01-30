18b3eb46d503f9c15d419d8328bab39135ef4135
Evan907cross <151195636+Evan907cross@users.noreply.github.com>
2026-01-30 08:46:37 -0900

    merged e_color to e_functions
    e_functions need more stuff, and e_color does not deserver there own file

--- scripts/e_functions.js
+++ scripts/e_functions.js
@@ -34,3 +34,7 @@ function HtmlSeperateUnit(val=0){
     }
 	return false
 }
+
+//RIP e_color
+const RGBtoHex = (r,g,b) => "#" + r.toString(16) + g.toString(16) + b.toString(16)
+const HextoRBG = (h) => [parseInt(h.substring(1,2),16),parseInt(h.substring(3,4),16),parseInt(h.substring(5,6),16)]
